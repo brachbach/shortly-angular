ffb806b6f840758ab164a7bf40c1545a557d8de6
brachbach <brachbach@gmail.com>
2016-05-21 15:04:48 -0700

    templates for link view

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,10 +1,12 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, $location, Links, Auth) {
+.controller('LinksController', function ($scope, $location, Links, Auth, LinkPrepService) {
   $scope.data = {};
-  Links.getAll().then(function(links) {
-    $scope.data.links = links;
-  });
+  $scope.data.links = LinkPrepService;
+  // console.log($scope.data);
+  // Links.getAll().then(function(links) {
+  //   $scope.data.links = links;
+  // });
   $scope.signout = function() {
     Auth.signout();
   };
@@ -13,5 +15,12 @@ angular.module('shortly.links', [])
   };
   $scope.shorten = function() {
     $location.path('/shorten');
+  };
+})
+.directive('shortenedLink', function() {
+  return {
+    template : '<div class="visits"><span class="count">{{link.visits}}</span>\
+     Visits</div><div class="title">{{link.title}}</div><div class="original">{{link.url}}</div>\
+     <a href="{{link.baseUrl}}/{{link.code}}">{{link.baseUrl}}/{{link.code}}</a>'
   };
 });
